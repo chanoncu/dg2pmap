8d7d12077f8b9b0ebd28c372eaea19d95b972fc0
chanoncu <116282656+chanoncu@users.noreply.github.com>
2022-10-21 22:42:18 +0700

    Update defaults.js

--- config/defaults.js
+++ config/defaults.js
@@ -34,10 +34,10 @@ define([], function () {
       "id": null, //id of the search layer as defined in the web map
       "fields": [] //Name of the string field to search
     },
-    "customUrlParam": null, //Name of url param. For example parcels
+    "customUrlParam": 1245243242343, //Name of url param. For example parcels
     "bingmapskey": "", //Enter the url to your organizations bing maps key if you want to use bing basemaps
     //Defaults to arcgis.com. Set this value to your portal or organization host name.
-    "sharinghost": location.protocol + "//" + "www.arcgis.com",
+    "sharinghost": location.protocol + "//" + "https://worleyparsons.maps.arcgis.com/",
     //When true the template will query arcgis.com for default settings for helper services, units etc. If you
     //want to use custom settings for units or any of the helper services set queryForOrg to false then enter
     //default values for any items you need using the helper services and units properties.
